f76be485707561174a3c4e05cc720e0ab1e80f45
Venilton FalvoJr <falvojr@gmail.com>
2016-05-28 11:22:47 -0300

    Initial commit Project 2

---
+++ Project2/src/main/java/br/usp/falvojr/dtw/Main.java
@@ -0,0 +1,14 @@
+package br.usp.falvojr.dtw;
+
+/**
+ * Main class.
+ *
+ * @author Venilton FalvoJr (falvojr)
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+	}
+
+}

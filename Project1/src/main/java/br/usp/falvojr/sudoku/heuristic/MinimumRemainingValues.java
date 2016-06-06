72081b3caa93d78eb0b5816c9907130ae5205c29
Venilton FalvoJr <falvojr@gmail.com>
2016-06-06 00:41:54 -0300

    Final refactoring

--- Project1/src/main/java/br/usp/falvojr/sudoku/heuristic/MinimumRemainingValues.java
+++ Project1/src/main/java/br/usp/falvojr/sudoku/heuristic/MinimumRemainingValues.java
@@ -17,63 +17,62 @@ import br.usp.falvojr.sudoku.util.SuDokus;
  */
 public class MinimumRemainingValues {
 
-    private final Comparator<String> comparator = (values1, value2) -> values1.length() - value2.length();
-    private List<String> usedKeys = new LinkedList<>();
+	private final Comparator<String> comparator = (values1, value2) -> values1.length() - value2.length();
+	private List<String> usedKeys = new LinkedList<>();
 
-    public void setUsedKeys(List<String> usedKeys) {
-	this.usedKeys = usedKeys;
-    }
-
-    public List<String> getUsedKeys() {
-	return this.usedKeys;
-    }
-
-    public List<String> getClonedUsedKeys() {
-	final List<String> newList = new LinkedList<>();
-	newList.addAll(this.usedKeys);
-	return newList;
-    }
-
-    public Map<String, String> init(Map<String, String> domains) {
-	this.usedKeys.clear();
-	return this.sortByDomains(domains);
-    }
-
-    public Map<String, String> sortByDomains(Map<String, String> domains) {
-	return SuDokus.sortByValue(domains, this.comparator);
-    }
-
-    public String getNextKey(Map<String, String> domains) {
-	final Set<String> keySet = domains.keySet();
-	for (final String key : keySet) {
-	    if (!this.usedKeys.contains(key)) {
-		return key;
-	    }
+	public void setUsedKeys(List<String> usedKeys) {
+		this.usedKeys = usedKeys;
 	}
-	return StringUtils.EMPTY;
-    }
 
-    public boolean hasNextKey(Map<String, String> domains) {
-	return StringUtils.isNotEmpty(this.getNextKey(domains));
-    }
+	public List<String> getUsedKeys() {
+		return this.usedKeys;
+	}
 
-    public void burnKey(String key) {
-	this.usedKeys.add(key);
-    }
+	public List<String> getClonedUsedKeys() {
+		final List<String> newList = new LinkedList<>();
+		newList.addAll(this.usedKeys);
+		return newList;
+	}
 
-    /**
-     * Private constructor for Singleton Pattern.
-     */
-    private MinimumRemainingValues() {
-	super();
-    }
+	public void init(Map<String, String> domains) {
+		this.usedKeys.clear();
+	}
 
-    private static class MinimumRemainingValuesHolder {
-	public static final MinimumRemainingValues INSTANCE = new MinimumRemainingValues();
-    }
+	public Map<String, String> sortByDomains(Map<String, String> domains) {
+		return SuDokus.sortByValue(domains, this.comparator);
+	}
 
-    public static MinimumRemainingValues getInstance() {
-	return MinimumRemainingValuesHolder.INSTANCE;
-    }
+	public String getNextKey(Map<String, String> domains) {
+		final Set<String> keySet = domains.keySet();
+		for (final String key : keySet) {
+			if (!this.usedKeys.contains(key)) {
+				return key;
+			}
+		}
+		return StringUtils.EMPTY;
+	}
+
+	public boolean hasNextKey(Map<String, String> domains) {
+		return StringUtils.isNotEmpty(this.getNextKey(domains));
+	}
+
+	public void burnKey(String key) {
+		this.usedKeys.add(key);
+	}
+
+	/**
+	 * Private constructor for Singleton Pattern.
+	 */
+	private MinimumRemainingValues() {
+		super();
+	}
+
+	private static class MinimumRemainingValuesHolder {
+		public static final MinimumRemainingValues INSTANCE = new MinimumRemainingValues();
+	}
+
+	public static MinimumRemainingValues getInstance() {
+		return MinimumRemainingValuesHolder.INSTANCE;
+	}
 
 }
